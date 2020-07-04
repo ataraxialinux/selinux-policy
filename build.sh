5c6d8c675506540709df8bddc2129e488b80c42c
Ishimoto Shinobu <nagakamira@gmail.com>
2020-07-04 20:15:50 +0300

    ok
    Signed-off-by: Ishimoto Shinobu <nagakamira@gmail.com>

--- build.sh
+++ build.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/sh
 
-mkdir -p $1/var/lib/selinux/mls $1/var/lib/selinux/targeted
-
-make bare -j
-make conf NAME=targeted TYPE=mcs DISTRO=redhat SYSTEMD=y UBAC=n MONOLITHIC=n DIRECT_INITRC=y UNK_PERMS=allow -j
-make base.pp NAME=targeted TYPE=mcs DISTRO=redhat SYSTEMD=y UBAC=n MONOLITHIC=n DIRECT_INITRC=y UNK_PERMS=allow -j
-make validate modules NAME=targeted TYPE=mcs DISTRO=redhat SYSTEMD=y UBAC=n MONOLITHIC=n DIRECT_INITRC=y UNK_PERMS=allow -j
-make install DESTDIR="$1" NAME=targeted TYPE=mcs DISTRO=redhat SYSTEMD=y UBAC=n MONOLITHIC=n DIRECT_INITRC=y UNK_PERMS=allow -j
-make install-appconfig DESTDIR="$1" NAME=targeted TYPE=mcs DISTRO=redhat SYSTEMD=y UBAC=n MONOLITHIC=n DIRECT_INITRC=y UNK_PERMS=allow -j
-make install-headers DESTDIR="$1" NAME=targeted TYPE=mcs DISTRO=redhat SYSTEMD=y UBAC=n MONOLITHIC=n DIRECT_INITRC=y UNK_PERMS=allow -j
-make load DESTDIR="$1" NAME=targeted TYPE=mcs DISTRO=redhat SYSTEMD=y UBAC=n MONOLITHIC=n DIRECT_INITRC=y UNK_PERMS=allow SEMODULE="semodule -p $1 -X 100 " -j
+mkdir -p $1/var/lib/selinux/mls
 
 make bare -j
 make conf NAME=mls TYPE=mls DISTRO=redhat SYSTEMD=y UBAC=n MONOLITHIC=n DIRECT_INITRC=y UNK_PERMS=deny -j
