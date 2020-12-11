6f950374c4486364c4db95b07c0681618b865e89
Ishimoto Shinobu <nagakamira@gmail.com>
2020-12-12 04:14:57 +0900

    targeted
    Signed-off-by: Ishimoto Shinobu <nagakamira@gmail.com>

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ main() {
 
 	msg "Preparing build environment"
 	rm -rf "$build"
-	mkdir -p "$srcs" "$root/var/lib/selinux/mls" "$root/etc/selinux"
+	mkdir -p "$srcs" "$root/var/lib/selinux/targeted" "$root/etc/selinux"
 
 	msg "Dwonloading sources"
 	cd "$srcs"
@@ -31,65 +31,32 @@ main() {
 	patch -Np1 -i "$stuff"/0001-Branch-update.patch
 	patch -Np1 -i "$stuff"/0002-various.patch
 	make -j clean
-	make -j NAME=mls TYPE=mls UBAC=n UNK_PERMS=deny $opts bare
-	make -j NAME=mls TYPE=mls UBAC=n UNK_PERMS=deny $opts conf
-	make -j NAME=mls TYPE=mls UBAC=n UNK_PERMS=deny $opts DESTDIR="$root" install  install-appconfig
-	make -j NAME=mls TYPE=mls UBAC=n UNK_PERMS=deny $opts SEMODULE="semodule -p $root -X 100 " DESTDIR="$root" load
+	make -j NAME=targeted TYPE=mcs UBAC=y UNK_PERMS=allow $opts bare
+	make -j NAME=targeted TYPE=mcs UBAC=y UNK_PERMS=allow $opts conf
+	make -j NAME=targeted TYPE=mcs UBAC=y UNK_PERMS=allow $opts DESTDIR="$root" install  install-appconfig
+	make -j NAME=targeted TYPE=mcs UBAC=y UNK_PERMS=allow $opts SEMODULE="semodule -p $root -X 100 " DESTDIR="$root" load
 
 	msg "Configuring SELinux policy"
-	cat > "$root"/etc/selinux/mls/setrans.conf <<-EOF
+	cat > "$root"/etc/selinux/targeted/setrans.conf <<-EOF
 		#
-		# Multi-Level Security translation table for SELinux
+		# Multi-Category Security translation table for SELinux
 		# 
 		# Uncomment the following to disable translation libary
 		# disable=1
 		#
-		# Objects can be labeled with one of 16 levels and be categorized with 0-1023 
-		# categories defined by the admin.
+		# Objects can be categorized with 0-1023 categories defined by the admin.
 		# Objects can be in more than one category at a time.
-		# Users can modify this table to translate the MLS labels for different purpose.
-		#
-		# Assumptions: using below MLS labels.
-		#  SystemLow
-		#  SystemHigh
-		#  Unclassified 
-		#  Secret with compartments A and B.
-		# 
-		# SystemLow and SystemHigh
+		# Categories are stored in the system as c0-c1023.  Users can use this
+		# table to translate the categories into a more meaningful output.
+		# Examples:
+		# s0:c0=CompanyConfidential
+		# s0:c1=PatientRecord
+		# s0:c2=Unclassified
+		# s0:c3=TopSecret
+		# s0:c1,c3=CompanyConfidentialRedHat
 		s0=SystemLow
-		s15:c0.c1023=SystemHigh
-		s0-s15:c0.c1023=SystemLow-SystemHigh
-
-		# Unclassified level
-		s1=Unclassified
-
-		# Secret level with compartments
-		s2=Secret
-		s2:c0=A
-		s2:c1=B
-
-		# ranges for Unclassified
-		s0-s1=SystemLow-Unclassified
-		s1-s2=Unclassified-Secret
-		s1-s15:c0.c1023=Unclassified-SystemHigh
-
-		# ranges for Secret with compartments
-		s0-s2=SystemLow-Secret
-		s0-s2:c0=SystemLow-Secret:A
-		s0-s2:c1=SystemLow-Secret:B
-		s0-s2:c0,c1=SystemLow-Secret:AB
-		s1-s2:c0=Unclassified-Secret:A
-		s1-s2:c1=Unclassified-Secret:B
-		s1-s2:c0,c1=Unclassified-Secret:AB
-		s2-s2:c0=Secret-Secret:A
-		s2-s2:c1=Secret-Secret:B
-		s2-s2:c0,c1=Secret-Secret:AB
-		s2-s15:c0.c1023=Secret-SystemHigh
-		s2:c0-s2:c0,c1=Secret:A-Secret:AB
-		s2:c0-s15:c0.c1023=Secret:A-SystemHigh
-		s2:c1-s2:c0,c1=Secret:B-Secret:AB
-		s2:c1-s15:c0.c1023=Secret:B-SystemHigh
-		s2:c0,c1-s15:c0.c1023=Secret:AB-SystemHigh
+		s0-s0:c0.c1023=SystemLow-SystemHigh
+		s0:c0.c1023=SystemHigh
 	EOF
 	cat > "$root"/etc/selinux/config <<-EOF
 		# This file controls the state of SELinux on the system.
@@ -98,9 +65,9 @@ main() {
 		#     permissive - SELinux prints warnings instead of enforcing.
 		#     disabled - No SELinux policy is loaded.
 		SELINUX=enforcing
-		# SELINUXTYPE= can take one of these values:
-		#     mls - Multi Level Security protection.
-		SELINUXTYPE=mls
+		# SELINUXTYPE= can take one of these three values:
+		#     targeted - Targeted processes are protected.
+		SELINUXTYPE=targeted
 	EOF
 
 	msg "Creating policy archive"
